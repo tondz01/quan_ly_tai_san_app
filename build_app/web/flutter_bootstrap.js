5a51d1f1b3f5db18f44f1354b6f3c51a35bca2db
tondt <tondt@dyno.vn>
2025-10-17 09:20:54 +0700

    update prod

--- build_app/web/flutter_bootstrap.js
+++ build_app/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3654561062"
+    serviceWorkerVersion: "3966231833"
   }
 });
