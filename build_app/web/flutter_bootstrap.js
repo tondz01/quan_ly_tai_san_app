2e17a832434fecb80d9b9159a0c4acdc9ccec80a
Tondt <thanhtonvk@gmail.com>
2025-11-01 08:52:14 +0700

    update prod

--- build_app/web/flutter_bootstrap.js
+++ build_app/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "510655961"
+    serviceWorkerVersion: "4135772875"
   }
 });
