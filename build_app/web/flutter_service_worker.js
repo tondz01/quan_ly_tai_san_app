526538587bb3e8610ddb90c1cb58807de3ec3b02
tondt <tondt@dyno.vn>
2025-11-28 11:00:57 +0700

    update prod

--- build_app/web/flutter_service_worker.js
+++ build_app/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "edc82f6ed3284a45d69a721cf46dac23",
+const RESOURCES = {"flutter_bootstrap.js": "e768629423610d65a407967291638b21",
 "version.json": "5722a7e0666e24b14b4da63c3f0fd8d3",
 "index.html": "abd789a99afc97c6ad744cbd87efd730",
 "/": "abd789a99afc97c6ad744cbd87efd730",
-"main.dart.js": "6d171bcc51c94aa95a1a51e7ed565762",
+"main.dart.js": "a6e3ac23b1dae6e334df5bd55cbd3d16",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
