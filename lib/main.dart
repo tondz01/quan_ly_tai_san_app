7359ad1efddc09aa44608d9df28c8e4c5e216072
ChienPn <ngocchienpnc@gmail.com>
2025-11-29 03:30:21 +0700

    update ver build

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class Config {
 
   static const String appVersion = String.fromEnvironment(
     'APP_VERSION',
-    defaultValue: '${environment}_0.0.13_25/11/2025',
+    defaultValue: '${environment}_0.0.14_29/11/2025',
   );
   static const String appBuild = String.fromEnvironment(
     'APP_BUILD',
