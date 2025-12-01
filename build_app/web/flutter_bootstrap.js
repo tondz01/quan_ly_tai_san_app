0262075f135ca010c98e6ac8ac67ec5ef27766e6
Chien.pn <chien.pn@digidinos.com>
2025-12-01 18:22:01 +0700

    update build v4

--- build_app/web/flutter_bootstrap.js
+++ build_app/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1382284424"
+    serviceWorkerVersion: "2696573000"
   }
 });
