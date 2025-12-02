bf6cbdc9322d8197f8094b8516efbe41b503c50e
Chien.pn <chien.pn@digidinos.com>
2025-12-02 15:54:20 +0700

    build/build_0212_0

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Config {
 
   static const String appVersion = String.fromEnvironment(
     'APP_VERSION',
-    defaultValue: '${environment}_0.1.3_01/12/2025',
+    defaultValue: '${environment}_0.1.4_02/12/2025',
   );
   static const String appBuild = String.fromEnvironment(
     'APP_BUILD',
