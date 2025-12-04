e8cf2067d45b72340dbe68d35dd24edcae60866f
ChienPn <ngocchienpnc@gmail.com>
2025-12-05 02:15:28 +0700

    build/build_prd_0512_v1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class Config {
 
   static const String appVersion = String.fromEnvironment(
     'APP_VERSION',
-    defaultValue: '${environment}_0.1.5_05/12/2025',
+    defaultValue: '${environment}_0.1.6_05/12/2025',
   );
   static const String appBuild = String.fromEnvironment(
     'APP_BUILD',
